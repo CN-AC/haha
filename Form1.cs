927a6e71fdb1c66930fd895ae0d71be6075813db
blake <98577190+CN-AC@users.noreply.github.com>
2022-01-30 22:25:51 +0800

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace k
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            z.Text = textBox1.Text + "憨憨憨憨憨憨憨憨憨憨憨憨憨憨憨憨";
+
+        }
+
+        private void z_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
